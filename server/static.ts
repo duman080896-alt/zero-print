5d7e82ffd7b59ddebbc3eaa4a8471e8f8eb5c443
duman080896-alt <duman080896@gmail.com>
2026-06-24 07:55:49 +0000

    Add vyshivka redirect to static page

--- server/static.ts
+++ server/static.ts
@@ -62,6 +62,7 @@ export function serveStatic(app: Express) {
     next();
   });
 
+  app.get("/uslugi/vyshivka", (req, res) => { res.redirect(301, "/uslugi/vyshivka/"); });
   app.use(express.static(distPath, {
     maxAge: "1y",
     etag: true,
